90511683a690945accfd0e2d6e4c5dd31681a13d
zenproperty <abouzeidroy@gmail.com>
2019-11-01 14:34:30 +0200

    Cart html

--- functions/services/costo_services.js
+++ functions/services/costo_services.js
@@ -14,24 +14,33 @@
 				var return_result = options.return_result;
 				var return_response = options.return_response;
 				var return_user_message = options.return_user_message;
-				// Fill the data
-				//var domain = 'http://localhost:8888/costowoocommerce/wp-json';
-				// var bearer_token = 'QOgV8JGlBR5ZpMf75dM5ScC9uPZw6DDp';
 
-				//Local RAZ machine config
-				// var domain = 'http://localhost/raz/listify/wp-json';
+				
+				//Local Roy machine config
+				var domain = 'http://localhost:8888/costowoocommerce/wp-json';
+				var endpoint_url = domain + endpoint;
+				var url = endpoint_url;
+				var headers = {
+					'Content-Type': 'text/plain'
+				};
+				var json_params = {
+					consumer_key: 'ck_88ef47c27b0c4bb988a2216cc5a5924c3599c98b',
+					consumer_secret: 'cs_348fe0b1cd6eba0ba31ed486d18e2415654f9475'
+					//'format' : get_config_data('default_api_format')
+				};
+				var Base64 = { _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", encode: function (e) { var t = ""; var n, r, i, s, o, u, a; var f = 0; e = Base64._utf8_encode(e); while (f < e.length) { n = e.charCodeAt(f++); r = e.charCodeAt(f++); i = e.charCodeAt(f++); s = n >> 2; o = (n & 3) << 4 | r >> 4; u = (r & 15) << 2 | i >> 6; a = i & 63; if (isNaN(r)) { u = a = 64 } else if (isNaN(i)) { a = 64 } t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a) } return t }, decode: function (e) { var t = ""; var n, r, i; var s, o, u, a; var f = 0; e = e.replace(/[^A-Za-z0-9\+\/\=]/g, ""); while (f < e.length) { s = this._keyStr.indexOf(e.charAt(f++)); o = this._keyStr.indexOf(e.charAt(f++)); u = this._keyStr.indexOf(e.charAt(f++)); a = this._keyStr.indexOf(e.charAt(f++)); n = s << 2 | o >> 4; r = (o & 15) << 4 | u >> 2; i = (u & 3) << 6 | a; t = t + String.fromCharCode(n); if (u != 64) { t = t + String.fromCharCode(r) } if (a != 64) { t = t + String.fromCharCode(i) } } t = Base64._utf8_decode(t); return t }, _utf8_encode: function (e) { e = e.replace(/\r\n/g, "\n"); var t = ""; for (var n = 0; n < e.length; n++) { var r = e.charCodeAt(n); if (r < 128) { t += String.fromCharCode(r) } else if (r > 127 && r < 2048) { t += String.fromCharCode(r >> 6 | 192); t += String.fromCharCode(r & 63 | 128) } else { t += String.fromCharCode(r >> 12 | 224); t += String.fromCharCode(r >> 6 & 63 | 128); t += String.fromCharCode(r & 63 | 128) } } return t }, _utf8_decode: function (e) { var t = ""; var n = 0; var r = c1 = c2 = 0; while (n < e.length) { r = e.charCodeAt(n); if (r < 128) { t += String.fromCharCode(r); n++ } else if (r > 191 && r < 224) { c2 = e.charCodeAt(n + 1); t += String.fromCharCode((r & 31) << 6 | c2 & 63); n += 2 } else { c2 = e.charCodeAt(n + 1); c3 = e.charCodeAt(n + 2); t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63); n += 3 } } return t } };
+
+
+
+
+				// var domain = 'http://localhost/raz/costobackend/wp-json';
 				// var json_params = {
-				// 	consumer_key:'ck_0297bee89940b8b22757944ba5ff8f3ee00ba4cb',
-				// 	consumer_secret:'cs_2ab2dc47d37af267ea23bf493fd96fed2fabbddd'
+				// 	consumer_key:'ck_f7e12b70566c54a6f0a1b974f4e92d59c618ed26',
+				// 	consumer_secret:'cs_6ddb045c5ced5968ddb2756ca39eafa946bc59c3'
 				// 	//'format' : get_config_data('default_api_format')
 				// };
 
-				var domain = 'http://localhost/raz/costobackend/wp-json';
-				var json_params = {
-					consumer_key:'ck_f7e12b70566c54a6f0a1b974f4e92d59c618ed26',
-					consumer_secret:'cs_6ddb045c5ced5968ddb2756ca39eafa946bc59c3'
-					//'format' : get_config_data('default_api_format')
-				};
+
 				//Online woocommerce.dekkanjeh.com credentiasl
 				// var domain = 'https://woocommerce.dekkanjeh.com/wp-json';
 				// var json_params = {
